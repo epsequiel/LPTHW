9a14c0cb9a0424ab90f0d66edc6995932404339b
epsequiel <epsequiel@gmail.com>
2016-10-08 23:09:06 -0300

    ejercicios 10 11 y 12

---
+++ ex11.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+
+# Ejercicio 11 pag 42
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weight?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+        age, height, weight)
+
+print "Ahora con '\%s'"
+#print "So, you're %d old, %f tall and %f heavy." % (
+#        age, height, weight)
---
+++ ex12.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+
+# Ejercicio 12 pag 44
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weight? ")
+
+print "So, you are %r old, %r tall and %r heavy." % (
+        age, height, weight)
